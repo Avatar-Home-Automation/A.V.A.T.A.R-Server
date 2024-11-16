df2a5999f6021c478184150e68ed8a7e84d0e2a8
A.V.A.T.A.R <avatar.home.automation@gmail.com>
2024-11-16 16:09:14 +0100

    4.2.0

--- update/4.2.0/lib/versioning/darwin/step-1.ps1
+++ update/4.2.0/lib/versioning/darwin/step-1.ps1
@@ -1,7 +1,7 @@
-Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
-Write-Host "█                  A.V.A.T.A.R SERVER VERSION UPDATE                █"
-Write-Host "█                           MacOS installer                         █"
-Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" -ForegroundColor DarkMagenta
+Write-Host "█                  A.V.A.T.A.R SERVER VERSION UPDATE                █" -ForegroundColor DarkMagenta
+Write-Host "█                           MacOS installer                         █" -ForegroundColor DarkMagenta
+Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" -ForegroundColor DarkMagenta
 # A.V.A.T.A.R 29/10/2024
 
 # We have time... 
@@ -20,9 +20,10 @@ if (Test-Path ./update-$version.log -PathType Leaf) {
 $ErrorActionPreference = "Stop"
 
 Start-Transcript -path ./update-$version.log -append
+Clear-Host
 
-Write-Host "⏳​ New server version: " -NoNewline  
-Write-Host "$version" -ForegroundColor Magenta
+Write-Host "⏳​ New server version: " -NoNewline -ForegroundColor DarkMagenta 
+Write-Host "$version" -ForegroundColor DarkRed 
 
 # Get Server URL from property
 $properties = Get-Content ../core/Avatar.prop -Encoding utf8 | ConvertFrom-Json
@@ -30,18 +31,18 @@ $property = $properties.repository
 $url = "https://github.com/"+$property+"/archive/master.zip"
 
 # Download master package
-Write-Host "⏳​ Downloading server master package from GitHub" -NoNewline 
+Write-Host "⏳​ Downloading server master package from GitHub" -NoNewline -ForegroundColor DarkMagenta
 Invoke-WebRequest -Uri $url -OutFile ./A.V.A.T.A.R-Server-master.zip
 Write-Host " done" -ForegroundColor Green
 Start-Sleep -Seconds 1
 
 # Unzip master package
 if (Test-Path ./newVersion-$version) {
-    Write-Host "⏳​ Removing old existing server master package" -NoNewline 
+    Write-Host "⏳​ Removing old existing server master package" -NoNewline -ForegroundColor DarkMagenta
     Remove-Item ./newVersion-$version -Recurse -Force
     Write-Host " done" -ForegroundColor Green
 }
-Write-Host "⏳​ Unziping server master package" -NoNewline 
+Write-Host "⏳​ Unziping server master package" -NoNewline -ForegroundColor DarkMagenta
 Expand-Archive -LiteralPath ./A.V.A.T.A.R-Server-master.zip -DestinationPath ./newVersion-$version -Force
 Write-Host " done" -ForegroundColor Green
 Start-Sleep -Seconds 1
@@ -54,18 +55,18 @@ Set-Location -Path $package
 # Test version type
 $installType = $null
 $del = $null
-Write-Host "⏳​ Update type: " -NoNewline  
+Write-Host "⏳​ Update type: " -NoNewline -ForegroundColor DarkMagenta 
 foreach($line in Get-Content "./README.txt"){
     If ($line -like "*new version*") {
-        Write-Host "New server executable version" -ForegroundColor Magenta
+        Write-Host "New server executable version" -ForegroundColor DarkRed
         $installType = "exe"
         $del = $True
     } elseif ($line -like "*requires packages*") {
-        Write-Host "New packages in node_modules directory" -ForegroundColor Magenta
+        Write-Host "New packages in node_modules directory" -ForegroundColor DarkRed
         $installType = "module"
         $del = $True
     } elseif ($line -like "*files to be copied*") {
-        Write-Host "Files only" -ForegroundColor Magenta
+        Write-Host "Files only" -ForegroundColor DarkRed
         $installType = "file"
     }
 }
@@ -79,7 +80,7 @@ if ($null -eq $installType) {
 # perform installation for exe and module only
 If (($installType -eq "exe") -or ($installType -eq "module")) {
 
-    Write-Host "⏳​ Electron version: " -NoNewline  
+    Write-Host "⏳​ Electron version: " -NoNewline -ForegroundColor DarkMagenta 
     $json_package = Get-Content ./package.json -Encoding utf8 | ConvertFrom-Json
     $electron_version = $json_package.devDependencies.electron
     if ($null -eq $electron_version ) {
@@ -88,15 +89,15 @@ If (($installType -eq "exe") -or ($installType -eq "module")) {
         exit
     } else {
         $electron_version = $electron_version.Substring(1, $electron_version.Length-1)
-        Write-Host $electron_version -ForegroundColor Magenta
+        Write-Host $electron_version -ForegroundColor DarkRed
     }
 
     If ($installType -eq "exe") {
-        Write-Host "⏳​ Installing Electron packager, please wait..." 
+        Write-Host "⏳​ Installing Electron packager, please wait..." -ForegroundColor DarkMagenta
         start-process -FilePath "npm" -ArgumentList "install", "--save-dev @electron/packager" -NoNewWindow -workingdirectory . -Wait 
         Write-Host "Electron packager installed" -ForegroundColor Green
         Start-Sleep -Seconds 1
-        Write-Host "⏳​ Creating a new A.V.A.T.A.R server application, please wait..." 
+        Write-Host "⏳​ Creating a new A.V.A.T.A.R server application, please wait..." -ForegroundColor DarkMagenta
         start-process -FilePath "npx" -ArgumentList "electron-packager", ".", "--electron-version=$electron_version", "--icon=./avatar.icns", "--out=./output" -NoNewWindow -workingdirectory . -Wait
         Write-Host "A.V.A.T.A.R application created" -ForegroundColor Green
 
@@ -114,7 +115,7 @@ If (($installType -eq "exe") -or ($installType -eq "module")) {
         $package = "$package/output/$output_platform"
         Start-Sleep -Seconds 1 
     } else {
-        Write-Host "⏳​ Installing node_modules packages, please wait..." 
+        Write-Host "⏳​ Installing node_modules packages, please wait..." -ForegroundColor DarkMagenta
         start-process -FilePath "npm" -ArgumentList "install" -NoNewWindow -workingdirectory . -Wait
         Write-Host "Node_modules packages installed" -ForegroundColor Green
         Start-Sleep -Seconds 1
@@ -126,7 +127,7 @@ Set-Location -Path $current_location
 # Remove node_modules & Chrome if mandatory to exclude bad version files
 if ($del -eq $True) {
     $ErrorActionPreference = 'SilentlyContinue'
-    Write-Host "⏳​ Removing node_modules directory"
+    Write-Host "⏳​ Removing node_modules directory" -ForegroundColor DarkMagenta
     Remove-Item ../node_modules -Recurse -Force
     If ((Test-Path "../node_modules") -eq $True) {
         Write-Host "> Unable to remove node_modules directory, wait 3 seconds and retry..." -ForegroundColor DarkRed
@@ -151,21 +152,21 @@ If ($installType -eq "exe") {
 
 $ErrorActionPreference = 'Stop'
 # Copy new version to the A.V.A.T.A.R client directory
-Write-Host "⏳​ Copying new version to the A.V.A.T.A.R server directory, please wait..." -NoNewline 
+Write-Host "⏳​ Copying new version to the A.V.A.T.A.R server directory, please wait..." -NoNewline -ForegroundColor DarkMagenta
 $omissions = [string[]]@("LICENSE","version","LICENSES.chromium.html")
 Copy-Item -Path "$package/*" -Exclude $omissions -Destination "$installPath" -Recurse -Force
 Write-Host " done" -ForegroundColor Green
 Start-Sleep -Seconds 1
 
 If (($installType -eq "exe") -or ($installType -eq "module")) {
-    Write-Host "⏳​ Installing Electron package in A.V.A.T.A.R application, please wait..." 
+    Write-Host "⏳​ Installing Electron package in A.V.A.T.A.R application, please wait..." -ForegroundColor DarkMagenta
     start-process -FilePath "npm" -ArgumentList "install", "--save-dev electron@$electron_version" -NoNewWindow -workingdirectory ".." -Wait
     Write-Host "Electron package installation done" -ForegroundColor Green
     Start-Sleep -Seconds 1
 }
 
 # Update Properties
-Write-Host "⏳​ Updating Properties" -NoNewline 
+Write-Host "⏳​ Updating Properties" -NoNewline -ForegroundColor DarkMagenta
 $file = Get-Content "../core/Avatar.prop" -Encoding utf8 | ConvertFrom-Json
 $file | ForEach-Object {if($_.version -notmatch $version){$_.version=$version}}
 $file | ConvertTo-Json -EscapeHandling EscapeHtml -depth 32 | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Out-File "../core/Avatar.prop"
@@ -178,7 +179,7 @@ Write-Host " done" -ForegroundColor Green
 Start-Sleep -Seconds 1
 
 # Update package.json
-Write-Host "⏳​ Updating package.json" -NoNewline 
+Write-Host "⏳​ Updating package.json" -NoNewline -ForegroundColor DarkMagenta
 $file = Get-Content "../package.json" -Encoding utf8 | ConvertFrom-Json
 $file | ForEach-Object {if($_.version -notmatch $version){$_.version=$version}}
 $file | ConvertTo-Json -EscapeHandling EscapeHtml -depth 32 | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Out-File "../package.json" 
@@ -188,7 +189,7 @@ Start-Sleep -Seconds 1
 $ErrorActionPreference = 'Ignore'
 
 try {
-    Write-Host "⏳​ Removing new version temporary files, please wait..." -NoNewline 
+    Write-Host "⏳​ Removing new version temporary files, please wait..." -NoNewline -ForegroundColor DarkMagenta
     Remove-Item ./newVersion-$version -Recurse -Force
     Start-Sleep -Seconds 1
 
@@ -212,19 +213,19 @@ Out-File -FilePath ./step-2.txt -InputObject $version-$end_install -Encoding utf
 
 # Restart the A.V.A.T.A.R server
 $ErrorActionPreference = 'Stop'
-Write-Host "⏳​ Restarting A.V.A.T.A.R server" -NoNewline 
+Write-Host "⏳​ Restarting A.V.A.T.A.R server" -NoNewline -ForegroundColor DarkMagenta
 $app_location = "$current_location/../../../../.."
 start-process -filePath "$app_location/A.V.A.T.A.R-Server.app" -workingdirectory "$app_location" -wait
 Write-Host " done" -ForegroundColor Green
 Start-Sleep -Seconds 1
 
-Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
-Write-Host "█                                                                              █"
-Write-Host "█    The new $version version installation has been successfully completed!       █"
-Write-Host "█                                                                              █"
-Write-Host "█       You may wish to consult the installation 'update-$version.log' file       █"
-Write-Host "█                             in the app/tmp directory                         █"
-Write-Host "█                                                                              █"
-Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" -ForegroundColor DarkMagenta
+Write-Host "█                                                                              █" -ForegroundColor DarkMagenta
+Write-Host "█    The new $version version installation has been successfully completed!       █" -ForegroundColor DarkMagenta
+Write-Host "█                                                                              █" -ForegroundColor DarkMagenta
+Write-Host "█       You may wish to consult the installation 'update-$version.log' file       █" -ForegroundColor DarkMagenta
+Write-Host "█                             in the app/tmp directory                         █" -ForegroundColor DarkMagenta
+Write-Host "█                                                                              █" -ForegroundColor DarkMagenta
+Write-Host "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" -ForegroundColor DarkMagenta
 
 Stop-Transcript
