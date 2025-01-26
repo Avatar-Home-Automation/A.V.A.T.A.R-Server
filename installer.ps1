5b0ebd9f58782fb1131578d2e076442b4442f4ef
A.V.A.T.A.R <avatar.home.automation@gmail.com>
2025-01-26 11:35:14 +0100

    5.2.0

--- installer.ps1
+++ installer.ps1
@@ -67,9 +67,9 @@ function Uninstall-ElectronPackager {
 function Install-Electron {
     param ($workingdirectory)
 
-    Write-Host "> Installing Electron version $electron, please wait..." -ForegroundColor DarkMagenta
-    start-process -FilePath $npm -ArgumentList "install", "--save-dev electron@$electron" -NoNewWindow -workingdirectory $workingdirectory -Wait
-    Write-Host "Electron package installation done" -ForegroundColor Green
+    Write-Host "> Installing npm packages, please wait..." -ForegroundColor DarkMagenta
+    start-process -FilePath $npm -ArgumentList "install" -NoNewWindow -workingdirectory $workingdirectory -Wait
+    Write-Host "npm packages installation done" -ForegroundColor Green
     Start-Sleep -Seconds 1
 }
 
